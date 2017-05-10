e79e96b2185415a5e740fd825dc62348fe126e8c
Eugenijus Sabaliauskas <e.sabaliauskas@gmail.com>
2017-05-10 17:11:41 +0300

    adding access to static files

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 var express = require('express');
 
 var app = express();
-
 var port = 5000;
+
+app.use(express.static('public'));
+app.use(express.static('src/views'));
 
 app.get('/', function(req, res){
     res.send('hello world!');
