4658f854995c6102d00be33b2d5d3eac4f15686b
Eugenijus Sabaliauskas <e.sabaliauskas@gmail.com>
2017-05-12 19:44:35 +0300

    3.4 Gulp, JSHint and JSCS

--- app.js
+++ app.js
@@ -6,14 +6,14 @@ var port = 5000;
 app.use(express.static('public'));
 app.use(express.static('src/views'));
 
-app.get('/', function(req, res){
+app.get('/', function (req, res) {
     res.send('hello world!');
-})
+});
 
-app.get('/books', function(req, res){
+app.get('/books', function (req, res) {
     res.send('hello books!');
-})
+});
 
-app.listen(port, function(err){
+app.listen(port, function (err) {
     console.log('running server on port ' + port);
 });
