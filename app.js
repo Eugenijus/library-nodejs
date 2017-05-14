c112c2803ff38ccde07eecbfd698c89dca94ca91
Eugenijus Sabaliauskas <e.sabaliauskas@gmail.com>
2017-05-15 00:45:42 +0300

    4.5 EJS

--- app.js
+++ app.js
@@ -4,10 +4,14 @@ var app = express();
 var port = process.env.PORT || 5000;
 
 app.use(express.static('public'));
-app.use(express.static('src/views'));
+//app.use(express.static('src/views'));
+
+app.set('views', './src/views');
+app.set('view engine', 'ejs');
 
 app.get('/', function (req, res) {
-    res.send('hello world!');
+    //res.send('hello world!');
+    res.render('index', {title:'Hello from render EJS',list:['vienas', 'du']});
 });
 
 app.get('/books', function (req, res) {
