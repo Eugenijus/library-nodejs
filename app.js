64b045206aba7877f1e358e4765137024995c91e
Eugenijus Sabaliauskas <e.sabaliauskas@gmail.com>
2017-05-16 00:19:36 +0300

    5.4 Rendering
    Rendering views/books.ejs

--- app.js
+++ app.js
@@ -10,23 +10,52 @@ app.use(express.static('public'));
 app.set('views', './src/views');
 app.set('view engine', 'ejs');
 
+var books = [
+        {
+            title: 'War and Peace',
+            genre: 'Historical Fiction',
+            author: 'Lev Nikolayevich Tolstoy',
+            read: false
+        },
+        {
+            title: 'The Wind in the Willows',
+            genre: 'Fantasy',
+            author: 'Kenneth Grahame',
+            read: false
+        },
+        {
+            title: 'Life On The Mississippi',
+            genre: 'History',
+            author: 'Mark Twain',
+            read: false
+        }
+    ];
+
 bookRouter.route('/')
     .get(function (req, res) {
-    res.send('Hello Books!');
-});
+        /*looks for books.ejs */
+        res.render('books', {
+            title: 'Books',
+            nav: [
+                {Link: '/Books', Text: 'Books'},
+                {Link: '/About', Text: 'Authors'}
+            ],
+            books: books
+        });
+    });
 
 bookRouter.route('/single')
     .get(function (req, res) {
-    res.send('Hello Single Book');
-});
+        res.send('Hello Single Book');
+    });
 
 app.use('/Books', bookRouter);
 
 app.get('/', function (req, res) {
     //res.send('hello world!');
     res.render('index', {
-        title:'Hello from render EJS',
-        nav:[
+        title: 'Hello from render EJS',
+        nav: [
             {Link: '/Books', Text: 'Books'},
             {Link: '/About', Text: 'Authors'}
         ]
