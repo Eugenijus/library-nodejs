5c76e93c9aff3fc5f27203e6f3210697c029ab93
Eugenijus Sabaliauskas <e.sabaliauskas@gmail.com>
2017-05-15 23:44:19 +0300

    5.3 Routing

--- app.js
+++ app.js
@@ -2,12 +2,25 @@ var express = require('express');
 
 var app = express();
 var port = process.env.PORT || 5000;
+var bookRouter = express.Router();
 
 app.use(express.static('public'));
 //app.use(express.static('src/views'));
 
 app.set('views', './src/views');
 app.set('view engine', 'ejs');
+
+bookRouter.route('/')
+    .get(function (req, res) {
+    res.send('Hello Books!');
+});
+
+bookRouter.route('/single')
+    .get(function (req, res) {
+    res.send('Hello Single Book');
+});
+
+app.use('/Books', bookRouter);
 
 app.get('/', function (req, res) {
     //res.send('hello world!');
